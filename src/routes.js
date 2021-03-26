6641465e2113baac6465e3f4245897a0446a0dab
Daniel Sanches <dsanches@ciandt.com>
2021-03-26 01:53:30 -0300

    Adicionando o EJS

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 const express = require("express")
 const routes = express.Router()
 
-const basePath = `${__dirname}/views`
+const views = `${__dirname}/views/`
 
-routes.get('/', (request, response) => response.sendFile(`${basePath}/index.html`))
-routes.get('/job', (request, response) => response.sendFile(`${basePath}/job.html`))
-routes.get('/job/edit', (request, response) => response.sendFile(`${basePath}/job-edit.html`))
-routes.get('/profile', (request, response) => response.sendFile(`${basePath}/profile.html`))
+routes.get('/', (request, response) => response.render(`${views}index`))
+routes.get('/job', (request, response) => response.render(`${views}job`))
+routes.get('/job/edit', (request, response) => response.render(`${views}job-edit`))
+routes.get('/profile', (request, response) => response.render(`${views}profile`))
 
 module.exports = routes
