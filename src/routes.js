0f9f4855582bf11f08a4c40d8a98ae1278a82284
Daniel Sanches <dsanches@ciandt.com>
2021-03-26 01:36:59 -0300

    Mapeando as rotas da aplicação

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const routes = express.Router()
 
-routes.get('/', (request, response) => {
-    return response.sendFile(__dirname + "/views/index.html")
-})
+const basePath = `${__dirname}/views`
+
+routes.get('/', (request, response) => response.sendFile(`${basePath}/index.html`))
+routes.get('/job', (request, response) => response.sendFile(`${basePath}/job.html`))
+routes.get('/job/edit', (request, response) => response.sendFile(`${basePath}/job-edit.html`))
+routes.get('/profile', (request, response) => response.sendFile(`${basePath}/profile.html`))
 
 module.exports = routes
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const server = express()
 const routes = require("./routes")
 
-server.use(express.static("public"))
+server.use(express.static('public'))
 server.use(routes)
 
-server.listen(3000, () => { console.log("rodando") })
+server.listen(3000, () => { console.log('rodando') })
