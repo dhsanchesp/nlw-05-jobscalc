5398825381df9518e428b91dfbf70b2a11361e31
Daniel Sanches <dh.sanchesp@gmail.com>
2021-04-01 00:23:13 -0300

    Criando método para atualizar o perfil

--- src/routes.js
+++ src/routes.js
@@ -20,8 +20,24 @@ const Profile = {
     },
 
     update(req, res) {
-      
+      const data = req.body;
 
+      const weeksPerYear = 52;
+
+      const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) /12
+      const weekTotalHours = data["hours-per-day"] * data["days-per-week"]
+
+      const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+
+      const valueHour = data["monthly-budget"] * monthlyTotalHours
+
+      Profile.data = {
+        ...Profile.data,
+        ...req.body,
+        "value-hour": valueHour 
+      }
+
+      return res.redirect('/profile')
     }
   }
 }
